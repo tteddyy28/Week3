ee65d6c2f7c325e08d23c23660af98ec8acde8a2
tedtessa <ttessa@student.bridgew.edu>
2020-09-15 09:44:11 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+def main():
+    print("This is a function")
+    print("This is the second line")
+    age = 5
+    dog_age = age * 7
+    print(dog_age)
+    fake_age = "29"
+    print(fake_age)
+    updated_fake_age = fake_age * 5
+    print(updated_fake_age)
+    print(10 /3)
+main()
